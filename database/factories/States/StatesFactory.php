d793aaaae625138b5e446728d936ae0011b22770
Claudio <Claudio.fernandes@avantis.edu.br>
2023-04-11 16:30:45 -0300

    Create factory states

---
+++ database/factories/States/StatesFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories\States;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\states\States>
+ */
+class StatesFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
