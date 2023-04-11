835c92d24bca2916948337bf2c5bea02118a3761
Claudio <Claudio.fernandes@avantis.edu.br>
2023-04-11 16:29:45 -0300

    Create factory cities

---
+++ database/factories/States/citiesFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories\States;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\states\cities>
+ */
+class citiesFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
