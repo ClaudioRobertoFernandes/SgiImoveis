3d0345f1f97b588656d1d534f8f7c6e94dab9166
Claudio <Claudio.fernandes@avantis.edu.br>
2023-04-19 10:17:42 -0300

    Creating building model

--- app/Models/Building/Buildings.php
+++ app/Models/Building/Buildings.php
@@ -18,4 +18,14 @@ class Buildings extends Model
         'created_at' => 'datetime',
         'updated_at' => 'datetime',
     ];
+
+    public static function getBuildings(int $owner_id)
+    {
+        $building = self::where('owner_id', $owner_id)->pluck('name', 'id');
+        if ($building->isEmpty()) {
+            return 'Nenhum edifício Cadastrado para este usuario';
+        }
+
+        return $building;
+    }
 }
